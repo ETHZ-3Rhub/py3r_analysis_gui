eab12b9502def80f539ec7aa19bc1c27ce0180d0
lort3R <thomas.lorimer@hest.ethz.ch>
2026-06-01 09:43:30 +0200

    Create smoke_test_tracking.bat

---
+++ smoke_test_tracking.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+set REPO=C:\Users\Student1\Documents\GitHub\py3r_analysis_gui
+set EXE=%REPO%\tracking_env\Scripts\py3r_pose.exe
+set MODELS=C:\Users\Student1\Documents\GitHub\BohacekLabPoseModels\pose_estimation
+set ENV_MODEL=%MODELS%\environment\environment_main
+set MOUSE_MODEL=%MODELS%\mouse\mouse_top_main
+set VIDEO_DIR=D:\py3r_analysis_input_tests\e
+set OUT_DIR=D:\py3r_analysis_input_tests\e_tracking_output
+
+echo --- Checking paths ---
+if not exist "%EXE%"          echo MISSING: %EXE%
+if not exist "%ENV_MODEL%"    echo MISSING: %ENV_MODEL%
+if not exist "%MOUSE_MODEL%"  echo MISSING: %MOUSE_MODEL%
+if not exist "%VIDEO_DIR%"    echo MISSING: %VIDEO_DIR%
+echo.
+
+mkdir "%OUT_DIR%" 2>nul
+
+echo --- Running tracking on all videos in %VIDEO_DIR% ---
+for %%F in ("%VIDEO_DIR%\*.mp4" "%VIDEO_DIR%\*.avi" "%VIDEO_DIR%\*.mov" "%VIDEO_DIR%\*.mkv") do (
+    echo.
+    echo Tracking: %%F
+    "%EXE%" track "%%F" --model "%ENV_MODEL%" "%MOUSE_MODEL%" --tracker fixed-instances --instances oft mouse_top --output-folder "%OUT_DIR%"
+    echo Exit code: %ERRORLEVEL%
+)
+
+echo.
+echo --- Done. Output in %OUT_DIR% ---
+pause
